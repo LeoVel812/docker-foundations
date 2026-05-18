349b3a54be7d038e2ed50eb73722e240410f5e83
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-17 20:48:01 -0600

    Setup custom local mongo img as a base for docker compose

---
+++ 2-docker-compose/8-mongo-script-custom-img/mongo-data/init.js
@@ -0,0 +1,32 @@
+db = db.getSiblingDB("lv-products-service");
+db.createCollection("products");
+db.products.insertMany([
+  {
+    name: "Samsung Galaxy S21",
+    price: 99.99,
+  },
+  {
+    name: "Motorola Moto G Power",
+    price: 49.99,
+  },
+  {
+    name: "Xiaomi Redmi Note 10 Pro",
+    price: 59.99,
+  },
+  {
+    name: "IPhone 12 Pro",
+    price: 119.99,
+  },
+  {
+    name: "IPhone 13 Pro",
+    price: 139.99,
+  },
+  {
+    name: "IPhone 14 Pro",
+    price: 169.99,
+  },
+  {
+    name: "IPhone 15 Pro",
+    price: 199.99,
+  },
+]);
